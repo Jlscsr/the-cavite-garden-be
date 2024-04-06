abf9c40eb3f11226c44c2df51a5a54db0eefa68c
Julius Caesar Raagas <raagasjuliuscaesar@gmail.com>
2024-04-06 22:26:08 +0800

    Initial Commit

---
+++ config/db_connect.php
@@ -0,0 +1,23 @@
+<?php
+require_once './config/load_env.php';
+
+$environment = $_ENV['ENVIRONMENT'];
+
+if ($environment == "production") {
+    $host = $_ENV['PROD_DB_HOST'];
+    $username = $_ENV['PROD_DB_USERNAME'];
+    $Password = $_ENV['PROD_DB_PASSWORD'];
+    $database = $_ENV['PROD_DB_NAME'];
+} else {
+    $host = $_ENV['DEV_DB_HOST'];
+    $username = $_ENV['DEV_DB_USERNAME'];
+    $Password = $_ENV['DEV_DB_PASSWORD'];
+    $database = $_ENV['DEV_DB_NAME'];
+}
+
+$connection = new mysqli($host, $username, $Password, $database);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Create database connection
+require_once './config/db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./public/tcg_logo.jpg" type="image/x-icon">
+    <title>The Cavite Garden</title>
+</head>
+
+<body>
+    <h1>Welcome</h1>
+</body>
+
+</html>
