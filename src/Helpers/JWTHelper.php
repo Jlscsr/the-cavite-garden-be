53cc2a0adc0f4f0b01a64f0e352299901852cb04
unknown <obognechristian@gmail.com>
2024-11-19 16:59:54 +0800

    Fixing hosting

--- src/Helpers/JWTHelper.php
+++ src/Helpers/JWTHelper.php
@@ -16,8 +16,8 @@ class JWTHelper
 
     public function __construct()
     {
-        $this->secret_key = getenv('JWT_SECRET_KEY') ?: $_ENV['JWT_SECRET_KEY'];
-        $this->hash_algorithm = getenv('JWT_HASH_ALGORITHM') ?: $_ENV['JWT_HASH_ALGORITHM'];
+        $this->secret_key = getenv('JWT_SECRET_KEY');
+        $this->hash_algorithm = getenv('JWT_HASH_ALGORITHM');
     }
 
     /**
