febb0bc6e24057a98608a9c9fae838b642b322d0
unknown <obognechristian@gmail.com>
2024-11-19 16:23:36 +0800

    Fixing hosting

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Router;
 
+print_r($_GET);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
